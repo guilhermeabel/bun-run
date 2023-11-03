d4c4536c147b827956347779e62a3091cf03abce
Guilherme Abel <guilhermeabel8@gmail.com>
2023-11-03 10:05:08 -0300

    refactor: index and test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,26 @@
 import { expect, test } from "bun:test";
 import CalculateCheckout from "../src/checkout/CalculateCheckout";
+import ProductRepository from "../src/infra/repository/ProductRepository";
+import TaxesGatewayHttp from "../src/checkout/TaxesGateway";
+import FreightGatewayHttp from "../src/checkout/FreightGateway";
 
 test("Should calculate order based on checkout input", async () => {
 
-	const checkout = new CalculateCheckout();
+	const productRepository = new ProductRepository();
+	const taxesGateway = new TaxesGatewayHttp();
+	const freightGateway = new FreightGatewayHttp();
+
+	// mock taxesGateway.getTaxesInformation
+	taxesGateway.getTaxesInformation = async (country) => {
+		return { country: country, percentage: 20 };
+	};
+
+	// mock freightGateway.getFreightInformation
+	freightGateway.getFreightInformation = async (country) => {
+		return { country: country, serviceFee: 5 };
+	};
+
+	const checkout = new CalculateCheckout(productRepository, taxesGateway, freightGateway);
 
 	const items = [
 		{ id: 1, quantity: 2 },
